df4251f56bc86343a96d7e995aa24eee05877a04
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2023-07-05 19:38:15 +1000

    Update figures.R

--- figures.R
+++ figures.R
@@ -35,6 +35,15 @@ hallmark_order <- hallmark_counts %>%
 data2$hallmark <- factor(data2$hallmark, levels = rev(hallmark_order))
 color_palette<-c("#2166AC","#B2182B","#D6604D","#F4A582","#FDDBC7","#e8ddb7","#999999","#D1E5F0","#92C5DE","#4393C3","#053061","#67001F","black")
 
+top_lncRNAs <- data2 %>%
+  group_by(lncRNA) %>%
+  summarise(total_count = sum(count)) %>%
+  top_n(8, total_count) %>%
+  pull(lncRNA)
+
+data2 <- data2 %>%
+  filter(lncRNA %in% top_lncRNAs)
+
 # Create the alluvial plot
 ggplot(data2, aes(y = count, axis1 = lncRNA, axis2 = hallmark)) +
   geom_alluvium(aes(fill = hallmark, alpha = count),curve_type = "cubic", width = 2/8, knot.pos = 1, reverse = FALSE) +
@@ -47,11 +56,4 @@ ggplot(data2, aes(y = count, axis1 = lncRNA, axis2 = hallmark)) +
 
 
 
-top_lncRNAs <- data2 %>%
-  group_by(lncRNA) %>%
-  summarise(total_count = sum(count)) %>%
-  top_n(8, total_count) %>%
-  pull(lncRNA)
 
-data2 <- data2 %>%
-  filter(lncRNA %in% top_lncRNAs)
