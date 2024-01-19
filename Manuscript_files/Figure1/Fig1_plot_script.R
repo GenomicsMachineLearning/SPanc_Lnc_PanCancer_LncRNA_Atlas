60fecb37efa69ad17d20fc86f71eb4a10cbbe03b
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2024-01-19 13:36:12 +1000

    Update Fig1_plot_script.R

--- Manuscript_files/Figure1/Fig1_plot_script.R
+++ Manuscript_files/Figure1/Fig1_plot_script.R
@@ -1,3 +1,47 @@
+########################  Fig1 barplot ############################
+data<-read.csv("lncAtlas_barplot_data", sep="\t", header=TRUE)
+d<-pivot_longer(
+  data = data,
+  cols = -Sample,
+  names_to = "LncRNAs",
+  values_to = "No. of lncRNAs"
+)
+d$Sample <- factor(d$Sample,levels=unique(d$Sample))
+
+ggplot(d, aes(x = Sample, y = d$`No. of lncRNAs`, fill = LncRNAs)) +
+  geom_bar(stat = "identity", position = "stack", color = "black") +
+  scale_fill_grey() +
+  theme_bw() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.ticks = element_line(color = "black"),panel.border = element_blank(),
+    panel.background = element_blank(),
+    legend.position = "bottom"
+  ) + theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)) + ylab("No. of LncRNAs")
+
+ggsave("stats_barplot_600dpi.png", plot = p, width = 8.3, height = 5, units = "in", dpi = 600)
+
+# or
+#hex codes google colors:  https://spreadsheet.dev/how-to-get-the-hexadecimal-codes-of-colors-in-google-sheets
+custom_colors <- c("Novel.uTARs" = "#46bdc6", "lncExpDB" = "#f9cb9c", "FANTOM" = "#b6d7a8", "FANTOM...lncExpDB" = "#ffe599")
+p<-ggplot(d, aes(x = Sample, y = `No. of lncRNAs`, fill = LncRNAs)) +
+  geom_bar(stat = "identity", position = "stack", color = "black") +
+  scale_fill_manual(values = custom_colors) +  # Set custom colors here
+  theme_bw() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.ticks = element_line(color = "black"),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    legend.position = "bottom"
+  ) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5)) +
+  ylab("No. of LncRNAs")
+
 
 ################################# Fig 1 Relative gene- utar counts violin ######################
 library(viridis)
