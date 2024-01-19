de0f4bf2deb6b506e1ff5bb7f0f3372e6d61ca13
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2024-01-19 16:27:43 +1000

    Delete Manuscript_files/Figure5/Data_analysis/Fig5_plots.R

--- Manuscript_files/Figure5/Data_analysis/Fig5_plots.R
+++
@@ -1,9 +0,0 @@
-####################################### Manhattan plot #################################
-m2<-read.csv("~/Downloads/PhD/Analysis/ST/QUAN/PolyA_FF/Vis15_HN//C1/Manhattan/qqman_data.txt",sep="\t",header=TRUE)
-uTARs<- grep("^chr", m$gene)
-uTARs <- m2[uTARs, ]
-cuTARs <- subset(uTARs, pval > 0.05 | pval < -0.05)
-uTARs<-uTARs$gene
-manhattan1(m2, chr = "chr", bp = "start", p = "Log2FC", snp = "gene",col = c("#e5e4e2", "#bebebe"),highlight1 = uTARs, highlight2 = cuTARs, logp = FALSE,ylim=c(-8,8), ylab="average log2FC",suggestiveline = F, genomewideline = F)
-abline(h = 2.5, col = "black", lty = 3)  # Red line
-abline(h = -2.5, col = "black", lty = 3)
