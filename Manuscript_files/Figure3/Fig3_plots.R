788a5c4a0361c0d9468b03c870de7516513dcbfa
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2024-06-06 18:34:47 +1000

    Update Fig3_plots.R

--- Manuscript_files/Figure3/Fig3_plots.R
+++ Manuscript_files/Figure3/Fig3_plots.R
@@ -4,10 +4,14 @@
 T<-read.csv("~/Downloads/Manuscript_final_figs/TCGA_HM_renamed_data.txt", sep="\t", header=TRUE)
 MT <- T[, -1]
 rownames(MT)<-T$Sample.ID
+#normalize
+dge <- DGEList(counts = MT)
+dge <- calcNormFactors(dge)
+norm_counts <- cpm(dge)
 Tanno<-as.data.frame(read.csv("~/Downloads/Manuscript_final_figs/TCGA_HM_Metadata_renamed.txt", sep="\t", header=TRUE))
 Tannom<- as.data.frame(as.matrix(Tanno[, -1]))
 rownames(Tannom)<-Tanno$uTAR
-pheatmap(log(MT+0.0001),cluster_rows=FALSE, annotation_row = Tannom, angle=45)
+pheatmap(log(norm_counts+0.0001),cluster_rows=FALSE, annotation_row = Tannom, angle=45)
 
 
 ### Supplementary 
