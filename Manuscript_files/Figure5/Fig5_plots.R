f0bbc4033fae430b7bb3a7f4d22bbc7bd2dfe550
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2025-09-09 18:24:30 +1000

    Update Fig5_plots.R

--- Manuscript_files/Figure5/Fig5_plots.R
+++ Manuscript_files/Figure5/Fig5_plots.R
@@ -1,11 +1,48 @@
-####################################### Manhattan plot #################################
-m2<-read.csv("/Users/ritanya/Downloads/Acral/v2/wdgeR_pseudo/qqman_data.txt",sep="\t",header=TRUE)
-uTARs<- grep("^uTAR", m2$gene)
-uTARs <- m2[uTARs, ]
-#cuTARs <- subset(uTARs, pval > 0.05 | pval < -0.05)
-cuTARs<-c("uTAR97","uTAR117","uTAR650","uTAR915","uTAR1224","uTAR1249","uTAR1274","uTAR157","uTAR295","uTAR337")
-#"cuTAR23054","cuTAR24389","cuTAR141997","cuTAR201736","cuTAR280980","cuTAR291293","cuTAR293520","cuTAR30875","cuTAR59116","cuTAR67350")
-uTARs<-uTARs$gene
-manhattan1(m2, chr = "chr", bp = "start", p = "avglogFC", snp = "gene",col = c("#e5e4e2", "#bebebe"),highlight1 = uTARs, highlight2 = cuTARs, logp = FALSE,ylim=c(-8,8), ylab="average log2FC",suggestiveline = F, genomewideline = F)
-abline(h = 2.5, col = "black", lty = 3)  # Red line
-abline(h = -2.5, col = "black", lty = 3)
+## DE cuTARs in anti-PD1 treatment (Acral melanoma)
+
+library(ggpubr)
+my_colors <- c("Pre" = "#967bb6", "Post" = "#fa8072")
+p<-read.csv("/Users/ritanya/Downloads/Acral/A3_Treatment/pseudo/new/Random10_pseudo_bulk.txt",sep="\t",header=TRUE)
+p$Condition <- factor(p$Condition, levels = c("Pre", "Post"))
+p <- subset(p, Cell_type != "Unknown")
+uTAR8337<-p[p$gene %in% "uTAR8337",] # good
+uTAR2929<-p[p$gene %in% "uTAR2929",] # good
+
+p1<-ggplot(uTAR8337, aes(x = reorder(Cell_type, -Expression), y = Expression, fill = Condition)) +
+  geom_boxplot() +
+  scale_fill_manual(values = my_colors) +  # Set manual colors
+  theme_minimal() +  # Minimal theme
+  theme(
+    plot.background = element_rect(fill = "white"),  # White background
+    panel.grid.major = element_blank(),  # Remove major grid lines
+    panel.grid.minor = element_blank(),  # Remove minor grid lines
+    panel.border = element_blank(),  # Remove panel border
+    axis.line = element_line(),  # Keep axis lines
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "bottom",  # Move legend to the bottom
+    axis.ticks = element_line()  # Keep tick lines
+  ) +
+  labs(x = "Cell Type", y = "Expression") +  # Customize axis labels
+  guides(fill = guide_legend(reverse = TRUE)) +  # Reverse legend order
+  stat_compare_means(aes(group = Condition))
+
+#combined facet
+combined_df <- rbind(uTAR2929,uTAR8337)
+combined_df$Source <- factor(rep(c("uTAR8337", "uTAR2929"), each = nrow(uTAR8337)))
+ggplot(combined_df, aes(x = reorder(Cell_type, -Expression), y = Expression, fill = Condition)) +
+  geom_boxplot() +
+  scale_fill_manual(values = my_colors) +  # Set manual colors
+  theme_minimal() +  # Minimal theme
+  theme(
+    plot.background = element_rect(fill = "white"),  # White background
+    panel.grid.major = element_blank(),  # Remove major grid lines
+    panel.grid.minor = element_blank(),  # Remove minor grid lines
+    panel.border = element_blank(),  # Remove panel border
+    axis.line = element_line(),  # Keep axis lines
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "bottom",  # Move legend to the bottom
+    axis.ticks = element_line()  # Keep tick lines
+  ) +
+  labs(x = "Cell Type", y = "Expression") +  # Customize axis labels
+  guides(fill = guide_legend(reverse = TRUE)) +  # Reverse legend order
+  facet_wrap(~ Source, ncol = 1, strip.position = "right")
