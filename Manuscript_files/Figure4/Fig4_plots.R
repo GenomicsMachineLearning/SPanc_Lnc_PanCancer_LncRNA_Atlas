115bf51a54e922d3979cc721bd0908ed2f1d7308
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2025-09-09 18:20:38 +1000

    Update Fig4_plots.R

--- Manuscript_files/Figure4/Fig4_plots.R
+++ Manuscript_files/Figure4/Fig4_plots.R
@@ -1,54 +1,11 @@
-################################# Moran's Alluvial ##############################################################################################################################
-setwd("/Users/ritanya/Downloads/PhD/Analysis/ST/QUAN/PolyA_FF/morans/cancer_genes_moran/")
-library(tidyverse)
-library(igraph)
-library(ggalluvial)
-data <- read.csv("HNC_HH_3conf.txt",sep="\t")  
-
-# Subset the data for counts greater than 50
-data2 <- subset(data, count > 50)
-
-
-# Calculate the total count for each hallmark
-hallmark_counts <- data2 %>%
-  group_by(hallmark) %>%
-  summarize(total_count = sum(count))
-
-# Reorder the hallmark factor levels based on the total count
-hallmark_order <- hallmark_counts %>%
-  arrange(desc(total_count)) %>%
-  top_n(8, total_count) %>%
-  pull(hallmark)
-
-lncRNA_counts <- data2 %>%
-  group_by(lncRNA) %>%
-  summarize(total_count = sum(count))
-
-lncRNA_order <- lncRNA_counts %>%
-  arrange(desc(total_count)) %>%
-  top_n(3, total_count) %>%
-  pull(lncRNA)
-
-data2 <- data2 %>%
-  filter(hallmark %in% hallmark_order)
-
-data2$hallmark <- factor(data2$hallmark, levels = rev(hallmark_order))
-# Reorder the lncRNA factor levels based on the count
-#data2$lncRNA <- reorder(data2$lncRNA, -data2$count)
-data2$lncRNA <- factor(data2$lncRNA, levels = rev(lncRNA_order))
-
-#color_palette<-c("#2166AC","#B2182B","#D6604D","#F4A582","#FDDBC7","#e8ddb7","#999999","#D1E5F0","#92C5DE","#4393C3","#053061","#67001F","brown")
-color_palette<-c("#2166AC","#B2182B","#D6604D","#F4A582","#FDDBC7","#e8ddb7","#999999","#D1E5F0")
-
-#color_paletteh<-c("#2166AC","#B2182B","#D6604D","#F4A582","#FDDBC7","#e8ddb7","#999999","#D1E5F0") #for 8 hallmarks
-# Create the alluvial plot
-ggplot(data2, aes(y = count, axis1 = lncRNA, axis2 = hallmark)) +
-  geom_alluvium(aes(fill = hallmark, alpha = count),curve_type = "cubic", width = 2/4, knot.pos = 1, reverse = FALSE) +
-  guides(fill = "none") +
-  geom_stratum(alpha = 0, width = 2/4, reverse = FALSE) +
-  geom_text(stat = "stratum", aes(label = after_stat(stratum),  fontface = "bold"), reverse = FALSE, size = 5, hjust = 0.5) +
-  scale_x_continuous(breaks = 1:2, labels = c("lncRNA", "Hallmark")) +
-  scale_fill_manual(values = rev(color_palette))+
-  theme(axis.text = element_text(size = 10)) #10 for top uTARs and width = 2/4
-# save pdf landscape 9x8
-
+####################################### Manhattan plot #################################
+m2<-read.csv("/Users/ritanya/Downloads/Acral/v2/wdgeR_pseudo/qqman_data.txt",sep="\t",header=TRUE)
+uTARs<- grep("^uTAR", m2$gene)
+uTARs <- m2[uTARs, ]
+#cuTARs <- subset(uTARs, pval > 0.05 | pval < -0.05)
+cuTARs<-c("uTAR97","uTAR117","uTAR650","uTAR915","uTAR1224","uTAR1249","uTAR1274","uTAR157","uTAR295","uTAR337")
+#"cuTAR23054","cuTAR24389","cuTAR141997","cuTAR201736","cuTAR280980","cuTAR291293","cuTAR293520","cuTAR30875","cuTAR59116","cuTAR67350")
+uTARs<-uTARs$gene
+manhattan1(m2, chr = "chr", bp = "start", p = "avglogFC", snp = "gene",col = c("#e5e4e2", "#bebebe"),highlight1 = uTARs, highlight2 = cuTARs, logp = FALSE,ylim=c(-8,8), ylab="average log2FC",suggestiveline = F, genomewideline = F)
+abline(h = 2.5, col = "black", lty = 3)  # Red line
+abline(h = -2.5, col = "black", lty = 3)
